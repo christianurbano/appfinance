3e23d8e25bd86fabc7790419973edcefb619520b
christian urbano <krris96@gmail.com>
2024-09-23 11:06:39 -0300

    aplicação dos icones e botao back

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { View } from "react-native";
-import ButtonMenu from "../components/Button";
 import { useNavigation } from "@react-navigation/native";
 
+import ButtonMenu from "../components/Button";
+import LogoStack from "../../assets/icon/Stocks.svg"
+import LogoFunds from "../../assets/icon/Funds.svg"
+import LogoPension from "../../assets/icon/Pensions.svg"
 export default function Home(){
     const navigation= useNavigation()
     return(
         <View>
             <ButtonMenu
+            icon={<LogoStack/>}
             title="Stocks"
             onPress={()=> navigation.navigate("Stocks")}/>
             <ButtonMenu
+            icon={<LogoFunds/>}
             title="Funds"
             onPress={()=> navigation.navigate("Funds")}/>
             <ButtonMenu
+            icon={<LogoPension/>}
             title="Pensions"
             onPress={()=> navigation.navigate("Pensions")}/>
         </View>
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,33 +4,47 @@ import Home from "../Home";
 import Stocks from "../Stocks";
 import Funds from "../Funds";
 import Pensions from "../Pensions";
-
+import BackIcon from "../../assets/icon/BackIcon.svg"
+import { TouchableOpacity } from "react-native";
 const Stack = createNativeStackNavigator()
 export default function Routes(){
     return(
         <Stack.Navigator
-            initialRouteName="Home">
-            
-            <Stack.Screen
-                name="Home"
-                component={Home}
-                options={{title: 'Initial'}}
-            />
-            <Stack.Screen
-                name="Stocks"
-                component={Stocks}
-                options={{title: 'Stocks'}}
-            />
-            <Stack.Screen
-                name="Funds"
-                component={Funds}
-                options={{title: 'Funds'}}
-            />
-            <Stack.Screen
-                name="Pensions"
-                component={Pensions}
-                options={{title: 'Pensions'}}
-            />
+            initialRouteName="Home"
+            >
+            <Stack.Group
+                screenOptions={({navigation})=> ({
+                    
+                    presentation:'modal',
+                    headerStyle: {backgroundColor:"#6F4DBF"},
+                    headerLeft: () =>(
+                        <TouchableOpacity onPress={()=> {navigation.goBack()}}>
+                                <BackIcon/> 
+                        </TouchableOpacity>
+                    ),
+                headerBackTitleVisible: false,})}
+            >
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                    options={{title: 'Home'}}
+                />
+                <Stack.Screen
+                    name="Stocks"
+                    component={Stocks}
+                    options={{title: 'Stocks'}}
+                />
+                <Stack.Screen
+                    name="Funds"
+                    component={Funds}
+                    options={{title: 'Funds'}}
+                />
+                <Stack.Screen
+                    name="Pensions"
+                    component={Pensions}
+                    options={{title: 'Pensions'}}
+                />
+            </Stack.Group>
         </Stack.Navigator>
     )
 }
